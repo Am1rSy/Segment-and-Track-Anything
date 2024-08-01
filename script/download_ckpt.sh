d727118d3f6b2d55e3294adba0473b2c82577a9f
Am1rSy <117784441+Am1rSy@users.noreply.github.com>
2024-08-01 13:34:41 +0100

    Update download_ckpt.sh for microSAM ckpt

--- script/download_ckpt.sh
+++ script/download_ckpt.sh
@@ -3,7 +3,7 @@ gdown --id '1QoChMkTVxdYZ_eBlZhK2acq9KMQZccPJ' --output ./ckpt/R50_DeAOTL_PRE_YT
 
 # download sam-ckpt
 wget -P ./ckpt https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
-
+wget -P ./ckpt https://zenodo.org/records/8250299/files/vit_h_lm.pth?download=1
 # download grounding-dino ckpt
 wget -P ./ckpt https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
 
