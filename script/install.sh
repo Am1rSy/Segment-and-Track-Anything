832bb335377aff221c46e9ff0213d4c5ee98f561
Am1rSy <117784441+Am1rSy@users.noreply.github.com>
2024-08-01 13:45:25 +0100

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -2,18 +2,18 @@
 cd sam; pip install -e .
 cd -
 
-# Install Grounding-Dino
-pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
+# # Install Grounding-Dino
+# pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
 
 # Install other lib
 pip install numpy opencv-python pycocotools matplotlib Pillow==9.2.0 scikit-image
 pip install gradio==4.39.0 zip gdown ffmpeg==1.4
 
-# Install Pytorch Correlation
-git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
-cd Pytorch-Correlation-extension
-python setup.py install
-cd -
+# # Install Pytorch Correlation
+# git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
+# cd Pytorch-Correlation-extension
+# python setup.py install
+# cd -
 
 # Install AST
 git clone https://github.com/YuanGongND/ast.git ast_master
