5ecabb094dd3cf23c5af214011e9b7fab71d6cfa
Am1rSy <117784441+Am1rSy@users.noreply.github.com>
2024-08-01 13:45:53 +0100

    Update download_ckpt.sh

--- script/download_ckpt.sh
+++ script/download_ckpt.sh
@@ -4,8 +4,9 @@ gdown --id '1QoChMkTVxdYZ_eBlZhK2acq9KMQZccPJ' --output ./ckpt/R50_DeAOTL_PRE_YT
 # download sam-ckpt
 wget -P ./ckpt https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
 wget -P ./ckpt https://zenodo.org/records/8250299/files/vit_h_lm.pth?download=1
-# download grounding-dino ckpt
-wget -P ./ckpt https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
+
+# # download grounding-dino ckpt
+# wget -P ./ckpt https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
 
 # download mit-ast-finetuned ckpt
 wget -O ./ast_master/pretrained_models/audio_mdl.pth https://www.dropbox.com/s/cv4knew8mvbrnvq/audioset_0.4593.pth?dl=1
